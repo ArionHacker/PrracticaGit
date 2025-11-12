ddf70014921ee503bbff534fdfd5bc8ff7516738
ASIER ARACIL LLEDO <aracil10030@gmail.com>
2025-11-12 16:32:24 +0100

    Version RadioButton

---
+++ ejTelegramaBienHecho/ejTelegramaBienHecho/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ejTelegramaBienHecho
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radio2 = new System.Windows.Forms.RadioButton();
+            this.rad1 = new System.Windows.Forms.RadioButton();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radio2
+            // 
+            this.radio2.AutoSize = true;
+            this.radio2.Location = new System.Drawing.Point(190, 278);
+            this.radio2.Name = "radio2";
+            this.radio2.Size = new System.Drawing.Size(67, 17);
+            this.radio2.TabIndex = 14;
+            this.radio2.TabStop = true;
+            this.radio2.Text = "Ordinario";
+            this.radio2.UseVisualStyleBackColor = true;
+            // 
+            // rad1
+            // 
+            this.rad1.AutoSize = true;
+            this.rad1.Location = new System.Drawing.Point(190, 255);
+            this.rad1.Name = "rad1";
+            this.rad1.Size = new System.Drawing.Size(63, 17);
+            this.rad1.TabIndex = 13;
+            this.rad1.TabStop = true;
+            this.rad1.Text = "Urgente";
+            this.rad1.UseVisualStyleBackColor = true;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(227, 318);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(187, 318);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Coste";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(187, 142);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(430, 93);
+            this.textBox1.TabIndex = 10;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(485, 252);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(132, 86);
+            this.btnCalcular.TabIndex = 9;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(184, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Texto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radio2);
+            this.Controls.Add(this.rad1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radio2;
+        private System.Windows.Forms.RadioButton rad1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
